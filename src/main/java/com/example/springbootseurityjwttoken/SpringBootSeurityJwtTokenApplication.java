708ee234a7d146b66cf4f3eca4f71b5a28ce2ada
daniel12142007 <danijelahatzanov@gmail.com>
2023-04-08 17:37:04 +0600

    I add

--- src/main/java/com/example/springbootseurityjwttoken/SpringBootSeurityJwtTokenApplication.java
+++ src/main/java/com/example/springbootseurityjwttoken/SpringBootSeurityJwtTokenApplication.java
@@ -18,7 +18,7 @@ public class SpringBootSeurityJwtTokenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSeurityJwtTokenApplication.class, args);
-        System.out.printf("Hello World!");
+        System.out.println("Hello World!");
     }
     @PostConstruct
     public void init(){
